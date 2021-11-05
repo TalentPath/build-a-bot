b6af9a99bb6cff36458040f4da22dcfbe954f089
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-05 14:00:13 -0500

    minor tweak

--- src/geocoding.js
+++ src/geocoding.js
@@ -5,7 +5,6 @@ const getCoords = async(location) => {
     options = `https://geocoding-api.open-meteo.com/v1/search?name=${location}`;
             
       let response = await axios.request(options);
-      console.log(response.data);
       return response.data;
 }
 
