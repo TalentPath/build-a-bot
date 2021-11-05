c6964fffe645fc663384c3a4b0769dde40495461
jacob <jacob@jacobs-mbp.lan>
2021-11-05 12:07:09 -0500

    added prompt functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+const prompt = require('prompt-sync')();
 const { talkToBot } = require('./talkToBot');
 
+let input = '';
+
 const a = async() => {
-    let x = await talkToBot("Hey there!");
-    console.log(x);
+    console.log(`
+    Hi, I'm your friendly chat-bot. Send me a message to get started.
+    Enter 'quit' to exit. :(
+    `);
+
+    let resp = '';
+    while(input.toLowerCase() != 'quit'){
+        input = prompt('');
+        resp = await talkToBot(input);
+        console.log(resp.cnt);
+    }
 }
 
 a();
