8f1337050111ead316e7a3d13d2b228e5c206ae7
Gustavo Mendoza <gustavomendoza@Gustavos-MBP.hsd1.tx.comcast.net>
2021-11-05 14:42:24 -0500

    gus_shuffle

---
+++ game.js
@@ -0,0 +1,11 @@
+// const axios = require("axios"); 
+// const prompt = require('prompt-sync')
+
+
+// axios.get(`https://opentdb.com/api.php?amount=5`).then(function(response){
+   
+//         console.log(`response.data`);
+//     }
+// );
+
+
---
+++ untitled folder/gus_shuffle.js
@@ -0,0 +1,16 @@
+function shuffleArry (array) {
+    
+    // While there remain elements to shuffle...
+
+
+
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+     
+  
+ 
+    }
+  
+    return array;
