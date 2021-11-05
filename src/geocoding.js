84c119db256cfa8e04f4da7b59fbb1229bc76783
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-05 13:59:54 -0500

    Moved file and added functionality

--- src/geocoding.js
+++ src/geocoding.js
@@ -1,10 +1,11 @@
 var axios = require("axios").default;
 
-const getCoords = async(message) => {
+const getCoords = async(location) => {
 
-    options = "https://geocoding-api.open-meteo.com/v1/search?name=Berlin";
+    options = `https://geocoding-api.open-meteo.com/v1/search?name=${location}`;
             
       let response = await axios.request(options);
+      console.log(response.data);
       return response.data;
 }
 
